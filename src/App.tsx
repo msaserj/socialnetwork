af98021e303052686957c42d6818758990741315
msa <serg.ks@gmail.com>
2022-04-06 16:54:05 +0300

    lessons 1-10

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className="app-wrapper">
+        <header className='header'>
+            <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/business-logo-design-template-78655edda18bc1196ab28760f1535baa_screen.jpg" alt="logo"/>
+        </header>
+        <nav className='nav'>
+            <div>
+                <a href="#">Profile</a>
+            </div>
+            <div>
+                <a href="#">Messages</a>
+            </div>
+            <div>
+                <a href="#">News</a>
+            </div>
+            <div>
+                <a href="#">Music</a>
+            </div>
+            <div>
+                <a href="#">Settings</a>
+            </div>
+        </nav>
+        <div className='content'>
+            <img src="" alt=""/>
+        </div>
     </div>
   );
 }
+
 
 export default App;
