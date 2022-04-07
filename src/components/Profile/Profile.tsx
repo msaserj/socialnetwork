13e8df69bdd1c50a4c36f09c1d7fc865b0ba7e61
msaserj <serg.ks@gmail.com>
2022-04-07 21:59:37 +0300

    correct Profile.module.css correct Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import {MyPosts} from "./MyPosts/MyPosts";
+import classes from './Profile.module.css'
 
 export const Profile = () => {
     return(
         <div>
             <h2>Main content</h2>
             <div>
-                <img src="http://location-la-batie-montsaleon.fr/layout/img/entete.jpg" alt="la_batie"/>
+                <img className={classes.bgc} src="http://location-la-batie-montsaleon.fr/layout/img/entete.jpg" alt="la_batie"/>
             </div>
             <div>
                 ava + description
