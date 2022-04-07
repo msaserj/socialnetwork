8959a8e6494b64c5a646cb999d5bbb0edbe23f6f
msa <serg.ks@gmail.com>
2022-04-07 16:02:37 +0300

    lesson 19-20 React-router-dom import {Routes, Route, BrowserRouter} from 'react-router-dom';

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,24 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Dialogs} from "./components/Profile/Dialogs/Dialogs";
+import {Profile} from "./components/Profile/Profile";
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+
 
 const App = () => {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className="app-wrapper-content">
-                <Dialogs/>
-                {/*<Profile/>*/}
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path="/dialogs" element={<Dialogs/>}/>
+                        <Route path="/profile" element={<Profile/>}/>
+                    </Routes>
+                </div>
             </div>
-
-        </div>
+        </BrowserRouter>
     );
 }
 export default App;
--- src/components/Profile/Dialogs/Dialogs.tsx
+++ src/components/Profile/Dialogs/Dialogs.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from './Dialogs.module.css'
 
 
+
 export const Dialogs = () => {
     return(
         <div className={classes.content}>
