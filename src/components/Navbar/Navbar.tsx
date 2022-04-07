2d399a44448449d24aecc3423c3b46db9b572ed5
msa <serg.ks@gmail.com>
2022-04-07 16:28:34 +0300

    lesson 20 React JS (NavLink) .item a:focus {     color: black; }

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from './Navbar.module.css'
 
 export const Navbar = () => {
     return(
         <nav className={classes.nav}>
             <div className={`${classes.item} ${classes.active}`}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile">Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs">Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <a>News</a>
+                <NavLink to="/news">News</NavLink>
             </div>
             <div className={classes.item}>
-                <a>Music</a>
+                <NavLink to="/music">Music</NavLink>
             </div>
             <div className={classes.item}>
-                <a>Settings</a>
+                <NavLink to="/setting">Settings</NavLink>
             </div>
         </nav>
     )
