8be3fcca7e30a4a2ea2710cc73dd67c57edd5375
msa <serg.ks@gmail.com>
2022-04-08 10:49:05 +0300

    lesson 20 NavLink type {isActive: boolean}

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,23 +2,26 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import classes from './Navbar.module.css'
 
+
+const setActive = ({isActive}: {isActive: boolean}) => isActive ? classes.activeLink : classes.inactiveLink;
+
 export const Navbar = () => {
     return(
         <nav className={classes.nav}>
-            <div className={`${classes.item} ${classes.active}`}>
-                <NavLink to="/profile">Profile</NavLink>
+            <div>
+                <NavLink to="/profile" className = {setActive}>Profile</NavLink>
             </div>
-            <div className={classes.item}>
-                <NavLink to="/dialogs">Messages</NavLink>
+            <div>
+                <NavLink to="/dialogs" className = {setActive}>Messages</NavLink>
             </div>
-            <div className={classes.item}>
-                <NavLink to="/news">News</NavLink>
+            <div>
+                <NavLink to="/news" className = {setActive} >News</NavLink>
             </div>
-            <div className={classes.item}>
-                <NavLink to="/music">Music</NavLink>
+            <div>
+                <NavLink to="/music" className = {setActive}>Music</NavLink>
             </div>
-            <div className={classes.item}>
-                <NavLink to="/setting">Settings</NavLink>
+            <div>
+                <NavLink to="/setting" className = {setActive}>Settings</NavLink>
             </div>
         </nav>
     )
