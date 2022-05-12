1e8dd3837a58e2b58a1c52f444f289ac3fecd66f
msa <serg.ks@gmail.com>
2022-05-12 19:00:10 +0300

    lesson 33 not comleted, broken index.tsx render.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {rernderEntireTree} from "./render";
 
+// export let rernderEntireTree = () => {
+//     ReactDOM.render(
+//
+//         <App />,
+//         document.getElementById('root')
+//     );
+// }
 
-
-ReactDOM.render(
-
-    <App />,
-  document.getElementById('root')
-);
+rerenderEntireTree();
