eb59d9f482aae0625d54905a534960bf93607785
msa <serg.ks@gmail.com>
2022-05-26 13:04:56 +0300

    41 lesson. dialogs-reducer.ts if to case

--- src/redux/dialogs-reducer.ts
+++ src/redux/dialogs-reducer.ts
@@ -3,12 +3,16 @@ import {ActionsType, DialogPageType} from "./state.js";
 
 export const dialogsReducer = (state: DialogPageType, action: ActionsType) => {
 
-    if (action.type === "UPDATE-NEW-MESSAGE-TEXT") {
-        state.newMessageState = action.newMessage;
-    } else if (action.type === "SEND-NEW-MESSAGE") {
-        let message = state.newMessageState;
-        state.newMessageState = "";
-        state.messages.push({id: v1(), message: message});
+    switch (action.type) {
+        case "UPDATE-NEW-MESSAGE-TEXT":
+            state.newMessageState = action.newMessage;
+            return state
+        case "SEND-NEW-MESSAGE":
+            let message = state.newMessageState;
+            state.newMessageState = "";
+            state.messages.push({id: v1(), message: message});
+            return state
+        default:
+            return state
     }
-    return state
 }
