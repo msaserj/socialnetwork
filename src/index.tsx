97529486c0370c700c6697c9d37a64ce132a41c3
msa <serg.ks@gmail.com>
2022-05-26 16:36:18 +0300

    42 lesson. new const state and dispatch from redux and store.subscribe(rerenderTree) //other render rerenderTree() //first render

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,52 @@ import './index.css';
 import ReactDOM from "react-dom";
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
-import {store} from "./redux/store";
+import {store} from "./redux/redux-store";
 
 
 export let rerenderTree = () => {
+    const state = store.getState();
+    const dispatch = store.dispatch
+
     ReactDOM.render(
         <BrowserRouter>
-            <App store={store}/>
+            <App store={state} dispatch={dispatch}/>
         </BrowserRouter>,
         document.getElementById('root') );
 }
 
-rerenderTree();
 
-store.subscribe(rerenderTree);
+
+
+store.subscribe(rerenderTree)
+
+rerenderTree()
+
+
+
+
+// type RootStateType = {
+//     dialogPage: Array<DialogPageType>
+//     post: Array<PostType>
+//     sidebar: Array<SidebarType>
+// }
+// type PostType = {
+//     id: string
+//     message: string
+//     likesCount: number
+// }
+
+// type DialogPageType = {
+//     dialogs: Array<DialogType>
+//     messages: Array<MessageType>
+//     newMessageState: string
+// }
+// type MessageType = {
+//     id: string
+//     message: string
+// }
+// type DialogType = {
+//     id: string
+//     name: string
+// }
+// type SidebarType = {}
