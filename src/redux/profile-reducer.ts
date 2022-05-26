7e3dd896130f6b7ea5fa073494d6313dfb097197
msa <serg.ks@gmail.com>
2022-05-26 13:28:24 +0300

    41 lesson. profile-reducer.ts move actionTypes

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -7,6 +7,20 @@ type PostType = {
     likesCount: number
 }
 
+export type ProfileActionsType = ReturnType<typeof addPostAC> | ReturnType<typeof updateNewPostAC>
+export const addPostAC = (newPost: string) => {
+    return {
+        type: "ADD-POST",
+        newPost: newPost
+    } as const
+}
+export const updateNewPostAC = (newPostText: string) => {
+    return {
+        type: "UPDATE-NEW-POST-TEXT",
+        newPostText: newPostText
+    } as const
+}
+
 export const profileReducer = (state: ProfilePageType, action: ActionsType) => {
     switch (action.type) {
         case "ADD-POST":
