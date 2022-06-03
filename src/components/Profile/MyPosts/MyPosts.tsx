686a375c4bab095b09edb1c60287a14b1464f95e
msaserj <serg.ks@gmail.com>
2022-06-03 14:38:02 +0300

    43. Container component. MyPosts clear up for MyPostsConntainer

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,8 +1,6 @@
 import React, {ChangeEvent} from "react";
 import classes from './MyPosts.module.css'
 import {Post} from "./Post/Post";
-import {addPostAC, updateNewPostAC} from "../../../redux/profile-reducer";
-import {DispatchStoreType} from "../../../redux/redux-store";
 
 
 type postType = {
@@ -12,20 +10,22 @@ type postType = {
 }
 type AppStateType = {
     posts: Array<postType>
-    // addPostCallBack: (postName: string) => void
     newPostText: string
-    dispatch: DispatchStoreType
+    updateNewPostText: (newPostText: string) => void
+    addPostHandler: () => void
 }
 
 export const MyPosts = (props: AppStateType) => {
+
     let postsElements = props.posts.map(el=> <Post id={el.id} message={el.message} likesCount={el.likesCount} /> )
-    console.log(props.newPostText)
-    const addPostHandler = () => {
-        props.dispatch(addPostAC(props.newPostText))
+
+    const onAddPost = () => {
+        props.addPostHandler()
     }
 
     function onPostChange(e: ChangeEvent<HTMLTextAreaElement>) {
-        props.dispatch(updateNewPostAC(e.currentTarget.value))
+        // props.dispatch(updateNewPostAC(e.currentTarget.value))
+        props.updateNewPostText(e.currentTarget.value)
     }
 
     return (
@@ -36,7 +36,7 @@ export const MyPosts = (props: AppStateType) => {
                     <textarea onChange={onPostChange} value={props.newPostText}>Add post</textarea>
                 </div>
                 <div>
-                    <button onClick={addPostHandler}>Add post</button>
+                    <button onClick={onAddPost}>Add post</button>
                 </div>
                 <div className={classes.item}>
                     <h3>New Post</h3>
