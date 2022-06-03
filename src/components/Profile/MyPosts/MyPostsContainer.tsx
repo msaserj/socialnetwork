425d104cd084282cbdeaeef3193d78b7dee4da2f
msaserj <serg.ks@gmail.com>
2022-06-03 14:36:39 +0300

    43. Container component MyPostsContainer.tsx for MyPosts.tsx all logic to container component

---
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {addPostAC, updateNewPostAC} from "../../../redux/profile-reducer";
+import {DispatchStoreType} from "../../../redux/redux-store";
+import {MyPosts} from "./MyPosts";
+
+type postType = {
+    id: string
+    message: string
+    likesCount: number
+}
+type AppStateType = {
+    posts: Array<postType>
+    newPostText: string
+    dispatch: DispatchStoreType
+}
+
+export const MyPostsContainer = (props: AppStateType) => {
+    const addPost = () => {
+        props.dispatch(addPostAC(props.newPostText))
+    }
+    function onPostChange(text: string) {
+        let action = updateNewPostAC(text)
+        props.dispatch(action)
+    }
+
+    return (
+        <MyPosts updateNewPostText={onPostChange} addPostHandler={addPost} posts={props.posts} newPostText={props.newPostText}/>
+    )
+}
