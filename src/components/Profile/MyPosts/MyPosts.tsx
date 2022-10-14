2df9ca817d11c31caf29d2867987df93d071fb1a
msa <serg.ks@gmail.com>
2022-10-14 18:05:00 +0400

    86-87. shouldComponentUpdate, PureComponent, memo

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -10,35 +10,46 @@ type MyPostType = {
     profilePage: ProfilePageType
 }
 
-export const MyPosts = (props: MyPostType) => {
+export class MyPosts extends React.PureComponent<MyPostType> {
+    // PureComponent as shouldComponentUpdate
+    // but for functional component use HOC React.memo() -> wrap in this hoc your component
 
-    let postsElements = props.profilePage.posts.map(el=> <Post id={el.id} message={el.message} likesCount={el.likesCount} /> )
+    // shouldComponentUpdate(nextProps: Readonly<MyPostType>, nextState: Readonly<{}>, nextContext: any): boolean {
+    //     return  nextProps !== this.props || nextState !== this.state
+    // }
 
-    const postOnClickHandler = () => {
-        props.addPostOnClick()
-    }
-    const postOnChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        // props.dispatch(updateNewPostAC(e.currentTarget.value))
-        props.newPostTextOnChange(e.currentTarget.value)
-    }
+    render() {
+        console.log("MyPosts render")
+        let postsElements = this.props.profilePage.posts.map(el => <Post id={el.id} message={el.message}
+                                                                         likesCount={el.likesCount}/>)
 
-    return (
-        <div className={classes.postsBlock}>
-            <h3>My posts</h3>
-            <div>
+        const postOnClickHandler = () => {
+            this.props.addPostOnClick()
+        }
+        const postOnChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+            // props.dispatch(updateNewPostAC(e.currentTarget.value))
+            this.props.newPostTextOnChange(e.currentTarget.value)
+        }
+
+        return (
+            <div className={classes.postsBlock}>
+                <h3>My posts</h3>
                 <div>
-                    <textarea onChange={postOnChangeHandler} value={props.profilePage.newTextState}>Add post</textarea>
-                </div>
-                <div>
-                    <button onClick={postOnClickHandler}>Add post</button>
-                </div>
-                <div className={classes.item}>
-                    <h3>New Post</h3>
-                </div>
-                <div className={classes.item}>
-                    {postsElements}
+                    <div>
+                        <textarea onChange={postOnChangeHandler}
+                                  value={this.props.profilePage.newTextState}>Add post</textarea>
+                    </div>
+                    <div>
+                        <button onClick={postOnClickHandler}>Add post</button>
+                    </div>
+                    <div className={classes.item}>
+                        <h3>New Post</h3>
+                    </div>
+                    <div className={classes.item}>
+                        {postsElements}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
