68c0d5047ea46a083ccde85de8e9b89807c3f7b1
msa <serg.ks@gmail.com>
2022-10-19 22:12:08 +0400

    97 - update profile

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -23,44 +23,53 @@ export const ProfileInfo = (props: ProfileType) => {
     console.log(props.isOwner)
 
     const loadPhotoHandler = (e: any) => {
-        if(e.target.files.length){
-           let file = e.target.files[0]
+        if (e.target.files.length) {
+            let file = e.target.files[0]
             props.savePhoto(file)
         }
     }
     return (
 
-
         <div>
-
             <div>
                 <img className={classes.bgc} src="http://location-la-batie-montsaleon.fr/layout/img/entete.jpg"
                      alt="la_batie"/>
             </div>
-            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} getStatus={props.getStatus}/>
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}
+                                    getStatus={props.getStatus}/>
             <div className={classes.descriptionBlock}>
                 {/*<img className={classes.userPhoto} src={data.photos ? data.photos.large : userPhoto} alt=""/>*/}
 
                 pic<img className={classes.userPhoto} src={data.photos != null || undefined
-                    ? data.photos.small : userPhoto} alt=""/>
+                ? data.photos.small : userPhoto} alt=""/>
 
                 {props.isOwner && <input type={"file"} onChange={loadPhotoHandler}/>}
 
-                <div>{data.fullName}</div>
-                <div><p>About me: {data.aboutMe}</p></div>
+                <div><b>{data.fullName}</b></div>
+                <div><b>About me: {data.aboutMe}</b></div>
                 {data.lookingForAJob && <div>Ищу работу! <br/> {data.lookingForAJobDescription}</div>}
-                    <p>Contacts:</p>
-                    <ul>
-                        {contacts && <li>facebook: {contacts.facebook}</li>}
-                        {contacts && <li>vk: {contacts.vk}</li>}
-                        {contacts && <li>instagram: {contacts.instagram}</li>}
-                        {contacts && <li>twitter: {contacts.twitter}</li>}
-                        {contacts && <li>youtube: {contacts.youtube}</li>}
-                        {contacts && <li>github: {contacts.github}</li>}
-                        {contacts && <li>mainLink: {contacts.mainLink}</li>}
-                        {contacts && <li>website: {contacts.website}</li>}
-                    </ul>
-                </div>
+                <p><b>Contacts:</b></p>
+                <ul>
+                    {Object.keys(contacts).map(key => {
+                        // @ts-ignore
+                        return <Contact key={key} contactTitle={key} contactValue={contacts[key]}/>
+                    })}
+                </ul>
 
+            </div>
         </div>)
+}
+
+type ContactType = {
+    contactTitle: string
+    contactValue: string
+}
+
+export const Contact: React.FC<ContactType> = ({contactTitle, contactValue}) => {
+    return (<li><b>{contactTitle}:</b> {contactValue}</li>)
+}
+
+
+export const ProfileData = () => {
+
 }
