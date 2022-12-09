3e2519bce9d4216983f54c1cb93b5faa2d18f996
msa <serg.ks@gmail.com>
2022-12-09 18:35:20 +0400

    feat: adding scss files mixins and variables

--- src/components/00-Common/Button/Button.tsx
+++ src/components/00-Common/Button/Button.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
 
-export const Button = () => {
+
+import React, {ButtonHTMLAttributes, DetailedHTMLProps, ReactNode} from 'react';
+import css from "./Button.module.scss";
+
+
+// тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
+type ButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+
+type PropsType = ButtonPropsType & {
+    children: ReactNode
+}
+
+export const Button: React.FC<PropsType> = (
+    {children, className, ...restProps}
+) => {
+
     return (
-        <div>
+        <button className={css.link} {...restProps} >{children}</button>
 
-        </div>
     );
 };
-
