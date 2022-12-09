f610f9c8fea4079ab7fe844f7126f9c1cc2021e9
msa <serg.ks@gmail.com>
2022-12-09 18:26:06 +0400

    feat: new components small preloader and common button

---
+++ src/components/00-Common/PreloaderSmall/PreloaderSmall.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import preload from "../../../assets/images/ajax-loader.gif"
+
+export  const PreloaderSmall = () => {
+    return (
+        <div>
+            <img src={preload} alt="smallLoader"/>
+        </div>
+    );
+};
+
