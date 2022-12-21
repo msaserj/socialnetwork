4e18b21a39f785866a6240919325e8a7be2a9019
msa <serg.ks@gmail.com>
2022-12-21 04:14:08 +0400

    fix: warnings in scss files

--- src/components/04-RightWidgets/RightWidgets.tsx
+++ src/components/04-RightWidgets/RightWidgets.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import css from "./RightWidgets.module.scss"
 import {MainWidget} from "../03-Main/MainWidget/MainWidget";
 import {SiteInfo} from "../03-Main/SideWidgets/SiteInfo/SiteInfo";
 import {HashBar} from "../03-Main/SideWidgets/HashTags/HashBar";
-
+import css from "./RightWidgets.module.scss"
 
 export const RightWidgets = () => {
     return (
