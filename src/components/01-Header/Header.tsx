be2ecebc376bb4ab753b2a15ae2a3367ba8f070c
msa <serg.ks@gmail.com>
2022-12-25 16:08:47 +0400

    fix: avatar neo012345

--- src/components/01-Header/Header.tsx
+++ src/components/01-Header/Header.tsx
@@ -4,6 +4,7 @@ import {NavLink} from "react-router-dom";
 import {DataType} from "../../redux/auth-reducer";
 import logo from "../../assets/images/logo.png"
 import {Nav} from "./Nav/Nav";
+import avaNeo from "../../assets/images/avaNeo.png"
 
 type HeaderType = {
     authHeader: DataType
@@ -26,7 +27,7 @@ export const Header = (props: HeaderType) => {
                     ? <NavLink to={'/registr'}>Registration</NavLink>
                     : <div className={css.profileBlock}>
                         <NavLink className={css.profile} to={"/profile/" + props.id}>
-                            <img className={css.ava} src={props.avatar ? props.avatar.small : ""} alt=""/>
+                            <img className={css.ava} src={props.avatar ? props.avatar.small : avaNeo} alt=""/>
                             <h3 className={css.name}>{props.name}</h3>
                         </NavLink>
                         <Nav logoutTC={props.logoutTC} id={props.id}/>
