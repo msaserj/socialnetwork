7babcb614abd985fa64f4db495738614eb3df2cc
msa <serg.ks@gmail.com>
2022-12-29 19:18:08 +0400

    add: footer refactor: side profile widget

--- src/components/05-Footer/Footer.tsx
+++ src/components/05-Footer/Footer.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import css from "./Footer.module.css"
+import css from "./Footer.module.scss"
 
 export  const Footer = () => {
     return (
-        <div className={css.footer}>
-            <h3>Foooooter</h3>
-        </div>
+        <footer className={css.footerBlock}>
+            <div className={css.footerContainer}>
+                <h2 className={css.title}>Sergei Minko</h2>
+                <div className={css.social} >
+                    <a href="https://github.com/msaserj">GitHub</a>
+                    <a href="https://www.linkedin.com/in/msaserj">LinkedIn</a>
+                    <a href="https://msaserj.ru">msaserj</a>
+                </div>
+                <h3 className={css.rights}>
+                    © 2022. This layout is borrowed from <a href="https://velikorodnov.com/dev/devmatebook/">Matebook</a>
+                </h3>
+            </div>
+        </footer>
     );
 };
 
