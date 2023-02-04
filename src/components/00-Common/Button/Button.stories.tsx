41d82e17532393295872c81ae9a19692521bb7e1
msa <serg.ks@gmail.com>
2023-02-04 04:41:32 +0400

    add: storybook

---
+++ src/components/00-Common/Button/Button.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {action} from "@storybook/addon-actions";
+
+import {Button} from "./Button";
+
+export default {
+    title: 'Button',
+    component: Button,
+    argTypes: {
+            disabled: ['true', 'false']
+    }
+}
+
+
+const changeCallBack = action("Button pressed")
+
+
+export const ButtonBaseExample = (args: any) => {
+    return <Button onClick={changeCallBack} {...args}/>
+}
+ButtonBaseExample.args = {
+    children: "Button",
+    disabled: false
+}
+export const ButtonDisabled = (props: any) => {
+    return <Button onClick={changeCallBack} disabled>Disabled button</Button>
+}
