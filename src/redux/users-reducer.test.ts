702b1bcea33801c144cabbacad0d1d3233518c6a
msa <serg.ks@gmail.com>
2023-06-18 19:26:01 +0400

    add: tests for users-reducer

--- src/redux/users-reducer.test.ts
+++ src/redux/users-reducer.test.ts
@@ -1,4 +1,17 @@
-import { followAC, unFollowAC, UsersPageType, usersReducer } from './users-reducer';
+import {
+  followAC,
+  setCurrentPageAC,
+  setFilterAC,
+  setPageSizeAC,
+  setTotalUsersCountAC,
+  setUsersAC,
+  sortAC,
+  toggleIsFetchingAC,
+  toggleIsFollowingAC,
+  unFollowAC,
+  UsersPageType,
+  usersReducer
+} from './users-reducer';
 
 let initialState: UsersPageType;
 beforeEach(() => {
@@ -55,4 +68,124 @@ test('length after delete should be decrement', () => {
   let newState = usersReducer(initialState, action);
 
   expect(newState.users[0].followed).toBe(false);
+});
+
+// fdfdf______________________________________________________________________________________________
+
+test('should follow a user', () => {
+  const userId = 1;
+  const action = followAC(userId);
+
+  const newState = usersReducer(initialState, action);
+
+  expect(newState.users[0].followed).toBe(true);
+});
+
+test('should unfollow a user', () => {
+  const userId = 2;
+  const action = unFollowAC(userId);
+
+  const newState = usersReducer(initialState, action);
+
+  expect(newState.users[1].followed).toBe(false);
+});
+
+test('should set users and sort them', () => {
+  const users = [
+    {
+      id: 4,
+      photos: { small: '', large: '' },
+      followed: false,
+      name: 'John',
+      status: 'Hello, world!',
+      location: { city: 'London', country: 'UK' }
+    },
+    {
+      id: 5,
+      photos: { small: 'https://thispersondoesnotexist.com/image', large: '' },
+      followed: true,
+      name: 'Emma',
+      status: 'In the garden',
+      location: { city: 'Paris', country: 'France' }
+    }
+  ];
+  const action = setUsersAC(users);
+
+  const newState = usersReducer(initialState, action);
+
+  expect(newState.users.length).toBe(2);
+  expect(newState.users[1].id).toBe(4); // Sorted based on photos.small existence
+  expect(newState.users[0].id).toBe(5);
+});
+
+test('should set the filter', () => {
+  const filter = { term: 'search', friend: true };
+  const action = setFilterAC(filter);
+
+  const newState = usersReducer(initialState, action);
+
+  expect(newState.filter).toEqual(filter);
+});
+
+test('should sort users by photos.small', () => {
+  const action = sortAC(undefined);
+
+  const newState = usersReducer(initialState, action);
+
+  expect(newState.users.every(user => user.photos.small === null)).toBe(true);
+});
+
+test('should set the current page', () => {
+  const currentPage = 3;
+  const action = setCurrentPageAC(currentPage);
+
+  const newState = usersReducer(initialState, action);
+
+  expect(newState.currentPage).toBe(currentPage);
+});
+
+test('should set the page size', () => {
+  const pageSize = 10;
+  const action = setPageSizeAC(pageSize);
+
+  const newState = usersReducer(initialState, action);
+
+  expect(newState.pageSize).toBe(pageSize);
+});
+
+test('should set the total users count', () => {
+  const totalCount = 100;
+  const action = setTotalUsersCountAC(totalCount);
+
+  const newState = usersReducer(initialState, action);
+
+  expect(newState.totalUsersCount).toBe(totalCount);
+});
+
+test('should toggle isFetching', () => {
+  const isFetching = true;
+  const action = toggleIsFetchingAC(isFetching);
+
+  const newState = usersReducer(initialState, action);
+
+  expect(newState.isFetching).toBe(isFetching);
+});
+
+test('should toggle isFollowing and update followingInProgress', () => {
+  const userId = 1;
+  const initialFollowingInProgress = [2, 3];
+  const initialStateWithFollowingInProgress: any = {
+    ...initialState,
+    followingInProgress: initialFollowingInProgress
+  };
+
+  const actionStart = toggleIsFollowingAC(true, userId);
+  const newStateStart = usersReducer(initialStateWithFollowingInProgress, actionStart);
+
+  expect(newStateStart.followingInProgress).toContain(userId);
+
+  const actionStop = toggleIsFollowingAC(false, userId);
+  const newStateStop = usersReducer(newStateStart, actionStop);
+
+  expect(newStateStop.followingInProgress).not.toContain(userId);
 });
