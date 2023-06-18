4729d023aaa2b55d9d23d7b8e269988914a49d2a
msa <serg.ks@gmail.com>
2023-06-18 18:38:47 +0400

    add: tests for profile-reducer

--- src/redux/profile-reducer.test.ts
+++ src/redux/profile-reducer.test.ts
@@ -1,4 +1,15 @@
-import { addPostOnClickAC, deletePostAC, ProfilePageType, profileReducer, UserProfileType } from './profile-reducer';
+import {
+  addPostOnClickAC,
+  deletePostAC,
+  editProfileAC,
+  newPostTextOnChangeAC,
+  ProfilePageType,
+  profileReducer,
+  savePhotoAC,
+  setResultCodeAC,
+  setStatusAC,
+  UserProfileType
+} from './profile-reducer';
 
 const initialState: ProfilePageType = {
   posts: [
@@ -18,7 +29,9 @@ test('new post should be added', () => {
 
   let newState = profileReducer(initialState, action);
 
-  expect(newState.posts.length).toBe(4);
+  expect(newState.posts.length).toBe(initialState.posts.length + 1);
+  expect(newState.posts[0].message).toBe('');
+  expect(newState.posts[0].likesCount).toBe(5);
 });
 
 test('length after delete should be decrement', () => {
@@ -27,4 +40,57 @@ test('length after delete should be decrement', () => {
   let newState = profileReducer(initialState, action);
 
   expect(newState.posts.length).toBe(2);
+});
+
+test('should update new post text', () => {
+  const newPostText = 'New post text';
+  const action = newPostTextOnChangeAC(newPostText);
+
+  const newState = profileReducer(initialState, action);
+
+  expect(newState.newTextState).toBe(newPostText);
+});
+
+test('should set status', () => {
+  const status = 'New status';
+  const action = setStatusAC(status);
+
+  const newState = profileReducer(initialState, action);
+
+  expect(newState.status).toBe(status);
+});
+
+test('should set photo', () => {
+  const photoFile = 'new-photo.jpg';
+  const action = savePhotoAC(photoFile);
+
+  const newState = profileReducer(initialState, action);
+
+  expect(newState.userProfile.photos).toBe(photoFile);
+});
+
+test('should set result code', () => {
+  const code = 200;
+  const action = setResultCodeAC(code);
+
+  const newState = profileReducer(initialState, action);
+
+  expect(newState.resultCode).toBe(code);
+});
+
+test('should delete a post', () => {
+  const postId = '2';
+  const action = deletePostAC(postId);
+
+  const newState = profileReducer(initialState, action);
+  expect(newState.posts.length).toBe(2);
+});
+
+test('should edit profile', () => {
+  const edit = true;
+  const action = editProfileAC(edit);
+
+  const newState = profileReducer(initialState, action);
+
+  expect(newState.edit).toBe(edit);
 });
